fa673cc90326a87374044f13f879007a439d7ef0
moyamush <moyashi0324@gmail.com>
2021-12-13 21:15:01 +0900

    front

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5050;
 app.listen(port);
 
-app.get('/', (req, res) => {
+app.get('https://moyamush-todoapp.herokuapp.com/api/user/', (req, res) => {
     try {
-	res.setHeader('Access-Control-Allow-Origin', 'https://moyamush-todoapp-frontend.herokuapp.com')
+	    req.setHeader('Access-Control-Allow-Origin', 'https://moyamush-todoapp-frontend.herokuapp.com')
         res.status(201).send('ok')
+        console.error("ok");
     } catch (error) {
         console.error(error)
     }
