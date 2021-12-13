a97c7f201c9068068dd520081265e5cf27efd941
moyamush <moyashi0324@gmail.com>
2021-12-13 20:55:30 +0900

    userapi

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ var port = process.env.PORT || 5050;
 app.listen(port);
 
 app.get('/', (res,) => {
-    res.set({ 'Access-Control-Allow-Origin': 'https://moyamush-todoapp.herokuapp.com/' }); // ここでヘッダーにアクセス許可の情報を追加
+    res.set({ 'Access-Control-Allow-Origin': 'https://moyamush-todoapp.herokuapp.com' }); // ここでヘッダーにアクセス許可の情報を追加
   });
 console.log('server started '+ port);
